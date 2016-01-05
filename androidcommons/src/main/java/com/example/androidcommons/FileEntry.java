377197c45e42920a1977f12f98696e1b6875cdf6
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2016-01-04 22:21:52 -0500

    Updated FileEntry class with ability to build filtered trees based on a set of regex file extensions

--- androidcommons/src/main/java/com/example/androidcommons/FileEntry.java
+++ androidcommons/src/main/java/com/example/androidcommons/FileEntry.java
@@ -6,13 +6,14 @@ import com.example.androidcommons.FileMetadata.FileType;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class FileEntry extends HashSet<FileEntry> {
 	private static final long serialVersionUID = 1594311386293439370L;
 
 	private static final String TAG = Helper.MAIN_TAG + "->FileEntry";
-	
+
 	private FileEntry parent;
 	private FileMetadata metadata;
 
@@ -20,15 +21,15 @@ public class FileEntry extends HashSet<FileEntry> {
 		this.parent = parent;
 		this.metadata = new FileMetadata(path);
 	}
-	
+
 	public FileEntry getParent() {
 		return this.parent;
 	}
-	
+
 	public void addChildEntry(FileEntry entry) {
 		this.add(entry);
 	}
-	
+
 	public FileEntry get(String childName) {
 		for(FileEntry child : this) {
 			if(child.getName().equals(childName))
@@ -36,7 +37,7 @@ public class FileEntry extends HashSet<FileEntry> {
 		}
 		return null;
 	}
-	
+
 	public String getName() {
 		return this.getFile().getName();
 	}
@@ -54,7 +55,7 @@ public class FileEntry extends HashSet<FileEntry> {
 			sb.append("/");
 		return sb.toString();
 	}
-	
+
 	public FileMetadata getFileMetadata() {
 		return this.metadata;
 	}
@@ -72,7 +73,7 @@ public class FileEntry extends HashSet<FileEntry> {
 		}
 		return sb.toString();
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
 		FileEntry entry = null;
@@ -84,12 +85,12 @@ public class FileEntry extends HashSet<FileEntry> {
 		}
 		return false;
 	}
-	
+
 	public File getFile() {
 		return this.getFileMetadata().getFile();
 	}
-	
-	
+
+
 	public long getSize() {
 		if(this.getFile().isDirectory()) {
 			return 0;
@@ -97,10 +98,10 @@ public class FileEntry extends HashSet<FileEntry> {
 		else
 			return this.getFile().length();
 	}
-	
+
 	public ArrayList<FileEntry> getFiles() {
 		this.buildTree();
-		
+
 		ArrayList<FileEntry> entryList = new ArrayList<FileEntry>();
 		entryList.add(this);
 		if(this.getFile().isDirectory()) {
@@ -115,45 +116,63 @@ public class FileEntry extends HashSet<FileEntry> {
 		}
 		return entryList;
 	}
-	
+
 	public static FileEntry buildTree(String path) {
 		FileEntry root = new FileEntry(null, path);
 		root.buildTree();
 		return root;
 	}
-	
+
 	public boolean buildTree() {
+		return _buildTree(null);
+	}
+
+	public boolean _buildTree(String[] extensions) {
 		TimeKeeper tk = new TimeKeeper();
 		tk.start();
 		boolean wasUpdated = false;
-		
+
 		File file = getFile();
-		
+
 		if(!file.isDirectory())
 			return false;
-		
+
+		HashSet<String> extensionsSet = null;
+		if(extensions != null) {
+			extensionsSet = new HashSet<String>();
+			for(String s : Arrays.asList(extensions)) {
+				extensionsSet.add(s.toLowerCase());
+			}
+		}
+
 		if(this.size() != file.listFiles().length) {
 			this.clear();
-//			ExecutorService executor = Executors.newFixedThreadPool(3);
 			for(File f : file.listFiles()) {
-				final FileEntry entry = new FileEntry(this, f.getAbsolutePath());
-				this.add(entry);
-				wasUpdated = true;
-//				executor.execute(new Runnable() {
-//					public void run() {
-//						entry.buildTree();
-//					}
-//				});
+				boolean validExtension = false;
+				if(extensionsSet != null) {
+					String ext = getFileExtension(f);
+					validExtension = extensionsSet.contains(ext.toLowerCase());
+				}
+				else
+					validExtension = true;
+				if(validExtension) {
+					final FileEntry entry = new FileEntry(this, f.getAbsolutePath());
+					this.add(entry);
+					wasUpdated = true;
+				}
 			}
-//			executor.shutdown();
-//			try {
-//				while(!executor.awaitTermination(100, TimeUnit.MILLISECONDS));
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
 		}
 		tk.stop();
 		Log.d(TAG + "->buildTree", "Time taken :" + tk);
 		return wasUpdated;
+	}
+
+	private String getFileExtension(File file) {
+		String name = file.getName();
+		try {
+			return name.substring(name.lastIndexOf(".") + 1);
+		} catch (Exception e) {
+			return "";
+		}
 	}
 }
