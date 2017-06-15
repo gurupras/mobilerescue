d726e106abf8aeb77305dc355d0b6a53dc411577
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-06-14 22:52:31 -0400

    Updated DownloadService

--- app/src/main/java/com/example/mobilerescue/DownloadService.java
+++ app/src/main/java/com/example/mobilerescue/DownloadService.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.example.androidcommons.AndroidApplication;
@@ -26,26 +27,48 @@ import static com.example.androidcommons.Helper.makeToast;
 
 public class DownloadService extends Thread implements Runnable {
 	private static final String TAG = Helper.createTag(DownloadService.class);
-	
-	public int port;
-
-	private ProgressDialog dialog;
-	private AtomicLong totalSize, bytesWritten;
-	private AtomicBoolean isCancelled;
-	private Activity activity;
 	private FileEntry root;
-	
+	public int port;
+	private ProgressDialog dialog;
+	private Activity activity;
+
+	private AtomicInteger numActiveDownloads = new AtomicInteger(0);
+
 	public DownloadService() {
 		this(AndroidApplication.externalPath.getAbsolutePath());
+		this.activity = AndroidApplication.getInstance().getCurrentActivity();
+
+		final AtomicBoolean dialogInitialized = new AtomicBoolean(false);
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				Log.v(TAG, "Attempting to initialize dialog");
+				dialog = new ProgressDialog(AndroidApplication.getInstance().getCurrentActivity());
+				synchronized(DownloadService.this) {
+					dialogInitialized.set(true);
+					DownloadService.this.notifyAll();
+				}
+				Log.v(TAG, "Dialog initialized");
+			}
+		});
+
+		synchronized(this) {
+			if(dialogInitialized.get()) {
+				// already initialized
+				Log.v(TAG, "Dialog was already initialized. No need to wait");
+			} else {
+				try {
+					Log.v(TAG, "Waiting for dialog to be initialized");
+					this.wait();
+					Log.v(TAG, "Finished waiting. Dialog initialized");
+				} catch (Exception e) {
+					Log.e(TAG, "Exception while waiting for notification: ", e);
+				}
+			}
+		}
 	}
 
 	public DownloadService(String basePath) {
-		this.activity = AndroidApplication.getInstance().getCurrentActivity();
-		this.dialog = new ProgressDialog(AndroidApplication.getInstance().getCurrentActivity());
 		this.root = new FileEntry(null, basePath);
-		this.totalSize = new AtomicLong(0);
-		this.bytesWritten = new AtomicLong(0);
-		this.isCancelled = new AtomicBoolean(false);
 		this.port = MainActivity.SettingsFragment.serverPort; 
 		
 		/* Build the tree */
@@ -67,7 +90,7 @@ public class DownloadService extends Thread implements Runnable {
 			init();
 			/* Start server and listen */
 			serverSocket = new ServerSocket();
-			serverSocket.setSoTimeout(1000);
+			serverSocket.setSoTimeout(10000);
 			serverSocket.setReuseAddress(true);
 			serverSocket.bind(new InetSocketAddress(port));
 			makeToast("Server running on port: " + port);
@@ -84,14 +107,24 @@ public class DownloadService extends Thread implements Runnable {
 					continue;
 				}
 				/* We now have a client. Handle it */
-				handleClient(clientSocket);
+				final Socket cSocket = clientSocket;
+				new Thread(new Runnable() {
+					public void run() {
+						try {
+							handleClient(cSocket);
+						} catch(Exception e) {
+							Log.e(TAG, "Failed to download", e);
+							e.printStackTrace();
+							makeToast("Terminating download service");
+						}
+					}
+				}).start();
 			}
 		} catch (Exception e) {
 			Log.e(TAG, "Failed to download", e);
 			e.printStackTrace();
 			makeToast("Terminating download service");
 		} finally {
-			dialog.cancel();
 			try {
 				serverSocket.close();
 			} catch(Exception e) {}
@@ -100,6 +133,7 @@ public class DownloadService extends Thread implements Runnable {
 	}
 
 	private void handleClient(Socket socket) throws Exception {
+		Log.d(TAG, "Handing new client connection...");
 		UploadRequestMessage urqm = new UploadRequestMessage();
 		urqm.parse(socket.getInputStream());
 		
@@ -131,49 +165,10 @@ public class DownloadService extends Thread implements Runnable {
 		}
 		socket.getOutputStream().write(ursm.build());
 		if(shouldReceive) {
-			totalSize.set(urqm.getFileSize());
-			activity.runOnUiThread(initDialog);
-			receiveFile(urqm, socket);
+			DownloadJob job = new DownloadJob(urqm, socket);
+			job.receiveFile();
 		}
 	}
-	private void receiveFile(UploadRequestMessage urqm, Socket socket) throws Exception {
-		// Try to do the networking part
-		String path = urqm.getPath();
-		File f = new File(path);
-		File parent = f.getParentFile();
-		try {
-			if(!parent.exists()) {
-				parent.mkdirs();
-			}
-		} catch(Exception e) {
-			Log.e(TAG, "Failed to create directories for file: " + f.getAbsolutePath());
-		}
-
-		FileOutputStream outstream = new FileOutputStream(f);
-		int defaultBufferSize = 1024 * 1024;
-		long offset = 0;
-		InputStream instream = socket.getInputStream();
-		while (offset < urqm.getFileSize()) {
-			int bufferSize = (int) (defaultBufferSize > (urqm.getFileSize() - offset) ?
-					(urqm.getFileSize() - offset) : defaultBufferSize);
-			ByteBuffer fileBuffer = ByteBuffer.allocate(bufferSize);
-			instream.read(fileBuffer.array());
-			outstream.write(fileBuffer.array());
-			bytesWritten.addAndGet(bufferSize);
-			offset += bufferSize;
-			activity.runOnUiThread(dialogUpdateRunnable);
-		}
-		dialog.dismiss();
-		instream.close();
-		outstream.close();
-		Log.d(TAG, "Finished obtaining file '" + f.getAbsolutePath() + "'");
-	}
-
-	private Runnable dialogUpdateRunnable = new Runnable() {
-		public void run() {
-			dialog.setProgress((int) ((bytesWritten.get() * 100) / totalSize.get()));
-		}
-	};
 
 	private Runnable initDialog = new Runnable() {
 		public void run() {
@@ -182,12 +177,99 @@ public class DownloadService extends Thread implements Runnable {
 			dialog.setIndeterminate(false);
 			dialog.setTitle("Transfer");
 			dialog.setCancelable(false);
+
+			/*
 			dialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
 					isCancelled.set(true);
 				}
 			});
-			dialog.show();
+			*/
 		}
 	};
+
+	private void showDialog() {
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				dialog.show();
+			}
+		});
+	}
+
+	private class DownloadJob {
+		private final String TAG = Helper.createTag(DownloadJob.class);
+
+		private AtomicLong totalSize, bytesWritten;
+		private AtomicBoolean isCancelled;
+		private Activity activity;
+		private UploadRequestMessage urqm;
+		private Socket socket;
+
+		DownloadJob(UploadRequestMessage urqm, Socket socket) {
+			this.urqm = urqm;
+			this.socket = socket;
+			this.activity = AndroidApplication.getInstance().getCurrentActivity();
+
+			this.totalSize = new AtomicLong(urqm.getFileSize());
+			this.bytesWritten = new AtomicLong(0);
+			this.isCancelled = new AtomicBoolean(false);
+		}
+
+		private Runnable dialogUpdateRunnable = new Runnable() {
+			public void run() {
+				dialog.setTitle("Transfer: " + urqm.getPath());
+				dialog.setProgress((int) ((bytesWritten.get() * 100) / totalSize.get()));
+			}
+		};
+
+		private void receiveFile() throws Exception {
+			// Try to do the networking part
+			String path = urqm.getPath();
+			File f = new File(path);
+			File parent = f.getParentFile();
+			try {
+				if(!parent.exists()) {
+					parent.mkdirs();
+				}
+			} catch(Exception e) {
+				Log.e(TAG, "Failed to create directories for file: " + f.getAbsolutePath());
+			}
+
+			activity.runOnUiThread(initDialog);
+
+			FileOutputStream outstream = new FileOutputStream(f);
+			int defaultBufferSize = 256 * 1024;
+			long offset = 0;
+			InputStream instream = socket.getInputStream();
+
+			int numDownloads;
+			synchronized (numActiveDownloads) {
+				if (numActiveDownloads.incrementAndGet() == 1) {
+					// Show dialog
+					showDialog();
+				}
+			}
+
+			while (offset < urqm.getFileSize()) {
+				int bufferSize = (int) (defaultBufferSize > (urqm.getFileSize() - offset) ?
+						(urqm.getFileSize() - offset) : defaultBufferSize);
+				ByteBuffer fileBuffer = ByteBuffer.allocate(bufferSize);
+				instream.read(fileBuffer.array());
+				outstream.write(fileBuffer.array());
+				bytesWritten.addAndGet(bufferSize);
+				offset += bufferSize;
+				activity.runOnUiThread(dialogUpdateRunnable);
+			}
+
+			synchronized (numActiveDownloads) {
+				if (numActiveDownloads.decrementAndGet() == 0) {
+					dialog.dismiss();
+				}
+			}
+
+			instream.close();
+			outstream.close();
+			Log.d(TAG, "Finished obtaining file '" + f.getAbsolutePath() + "'");
+		}
+	}
 }
